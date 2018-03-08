0204c092a6e13f7c52aea37adc99f1e00ced1391
Isaiah Gaspar <igaspar@asu.edu>
2018-03-08 15:13:40 -0700

    Add files via upload
    Chapter 2 "Try It Yourself" responses.

---
+++ chapter2_pcc/hello_world_ch2.py
@@ -0,0 +1,35 @@
+################ 2-1 ##################
+
+#message = "hello world";
+#print(message);
+
+################ 2-2 ##################
+
+#message = "hello world!";
+#print(message);
+
+#message = "hola mundo!";
+#print(message);
+
+################ 2-3 ##################
+
+#my_name = "isaiah";
+#message = "Hello " + my_name + ", would you like to lean some python?"
+#print(message);
+
+################ 2-4 ##################
+
+#my_name = "isaiah";
+#print(my_name.upper());
+#print(my_name.lower());
+#print(my_name.title());
+
+################ 2-5 ##################
+
+
+
+
+
+
+
+
