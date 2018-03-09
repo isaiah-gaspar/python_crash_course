904c460fa224d2712b149a28151e0c7b16c508d0
Isaiah Gaspar <igaspar@asu.edu>
2018-03-09 13:31:50 -0700

    Update hello_world_ch2.py
    Completed Chapter 2 examples.

--- chapter2_pcc/hello_world_ch2.py
+++ chapter2_pcc/hello_world_ch2.py
@@ -1,30 +1,101 @@
 ################ 2-1 ##################
 
-#message = "hello world";
-#print(message);
+#message = "hello world"
+#print(message)
+
 
 ################ 2-2 ##################
 
-#message = "hello world!";
-#print(message);
 
-#message = "hola mundo!";
-#print(message);
+#message = "hello world!"
+#print(message)
+
+#message = "hola mundo!"
+#print(message)
+
 
 ################ 2-3 ##################
 
+#Concatenating strings
+
+
 #my_name = "isaiah";
 #message = "Hello " + my_name + ", would you like to lean some python?"
-#print(message);
+#print(message)
+
 
 ################ 2-4 ##################
 
+#formating case of strings
+
+
 #my_name = "isaiah";
-#print(my_name.upper());
-#print(my_name.lower());
-#print(my_name.title());
+#print(my_name.upper())
+#print(my_name.lower())
+#print(my_name.title())
+
 
 ################ 2-5 ##################
+
+
+#message_p1 = 'Albert Einstein once said, "A person who never made a '
+#message_p2 = 'mistake never tried anything new"'
+
+#print(message_p1 + message_p2)
+
+
+################ 2-6 ##################
+
+
+#famous_person = "Napolean Hill"
+#message_p1 = '"The starting point of all achievment is DESIRE"'
+
+#print(famous_person + ", " + message_p1)
+
+
+################ 2-7 ##################
+
+
+#my_name1_ws = "  Isaiah\n"
+#my_name2_ws = "\t Gaspar  "
+
+#print(my_name1_ws + my_name2_ws)
+
+#print(my_name1_ws.lstrip() + my_name2_ws.strip())
+
+
+################ 2-8 ##################
+ 
+#Result of 8
+
+
+#print(4+4)
+#print(8-0)
+#print(2**3)
+#print(4*2)
+#print(16/2)
+
+
+################ 2-9 ##################
+
+#fav_num = 3;
+#message = "My favorite number is "
+
+#print(message + str(fav_num) + "!")
+
+################ 2-10 ##################
+
+#This is a comment
+
+
+
+
+
+
+
+
+
+
 
 
 
