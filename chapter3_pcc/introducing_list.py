9656fa60cde457079d291d678d1e3e4bf266019f
Isaiah Gaspar <igaspar@asu.edu>
2018-03-09 13:35:15 -0700

    Add files via upload
    [in-progress] Chapter 3 example problems.

---
+++ chapter3_pcc/introducing_list.py
@@ -0,0 +1,17 @@
+
+########## 3-1 ##########
+#store names and print in a list called names one at a time
+
+
+names = ['isaiah', 'hector', 'diego', 'matt']
+#
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+
+########## 3-2 ##########
+#Print names with a message
+
+
+
